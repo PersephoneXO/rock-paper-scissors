4f4577881e810c57446a5be6dfb550ba5720fcac
Persephone <134444980+PersephoneXO@users.noreply.github.com>
2023-11-21 14:10:22 -0500

    basic browser game works

--- javascript.js
+++ javascript.js
@@ -1,3 +1,5 @@
+//const prompt = require("prompt-sync")();
+
 function getComputerChoice(){
     let randomNum=Math.floor(Math.random()*3)
 
@@ -15,53 +17,65 @@ function getComputerChoice(){
 
 function playRound(playerSelection, computerSelection) {
     // your code here!
-
+    let resultMessage='';
+    const eachResultMessage=document.getElementById('each-result-message');
 
     let player=playerSelection.toLowerCase();
     if(player=='rock'){
         if(computerSelection=='rock'){
-            console.log("Tie!");
+            resultMessage="Tie!";
+            eachResultMessage.textContent=resultMessage;
             return 0;
         }
         if(computerSelection=='paper'){
-            console.log("You Lose! Paper beats Rock");
+            resultMessage="You Lose! Paper beats Rock";
+            eachResultMessage.textContent=resultMessage;
             return 1;
         }
         else{
-            console.log("You Win! Rock beats Scissors");
+            resultMessage="You Win! Rock beats Scissors";
+            eachResultMessage.textContent=resultMessage;
             return 2;
         }
     }
 
     if(player=='paper'){
         if(computerSelection=='paper'){
-            console.log("Tie!");
+            resultMessage="Tie!";
+            eachResultMessage.textContent=resultMessage;
             return 0;
         }
         if(computerSelection=='scissors'){
-            console.log("You Lose! Scissors beats Paper");
+            resultMessage="You Lose! Scissors beats Paper";
+            eachResultMessage.textContent=resultMessage;
             return 1;
         }
         else{
-            console.log("You Win! Paper beats Rock");
+            resultMessage="You Win! Paper beats Rock";
+            eachResultMessage.textContent=resultMessage;
             return 2;
         }
     }
 
     if(player=='scissors'){
         if(computerSelection=='scissors'){
-            console.log("Tie!");
+            resultMessage="Tie!";
+            eachResultMessage.textContent=resultMessage;
             return 0;
         }
         if(computerSelection=='rock'){
-            console.log("You Lose! Rock beats Scissors");
+            resultMessage="You Lose! Rock beats Scissors";
+            eachResultMessage.textContent=resultMessage;
             return 1;
         }
         else{
-            console.log("You Win! Scissors beats Paper");
+            resultMessage="You Win! Scissors beats Paper";
+            eachResultMessage.textContent=resultMessage;
             return 2;
         }
     }
+
+
   }
 
 
@@ -105,4 +119,69 @@ function game(){
     }
 }
 
-game();
+
+//gameV2() is for playing the game with the buttons
+function gameV2(){
+    let playerScore=0;
+    let computerScore=0;
+    let currentRound=0;
+
+    const pickRock=document.querySelector('#rock-button');
+    const pickPaper=document.querySelector('#paper-button');
+    const pickScissors=document.querySelector('#scissors-button');
+
+    pickRock.addEventListener('click',()=>{
+        playRoundV2('rock');
+    });
+    pickPaper.addEventListener('click',()=>{
+        playRoundV2('paper');
+    });
+    pickScissors.addEventListener('click',()=>{
+        playRoundV2('scissors');
+    });
+
+    function playRoundV2(playerSelection){
+        if (playerScore!=5&&computerScore!=5){
+        let computerSelection=getComputerChoice();
+        let played=playRound(playerSelection,computerSelection);
+        if (played==0){
+
+        }
+        else if(played==1){
+            computerScore+=1;
+        }
+        else{
+            playerScore+=1;
+        }
+
+        const currentPScore=document.getElementById('playerScore');
+        const currentCScore=document.getElementById('computerScore');
+        currentPScore.textContent=`Player Score: ${playerScore}`;
+        currentCScore.textContent=`Computer Score: ${computerScore}`;
+
+        currentRound+=1;
+
+        if(playerScore==5||computerScore==5){
+            endGame();
+        }
+    }
+    }
+
+    function endGame(){
+
+        const resultContainer=document.getElementById('final-results');
+
+        if(playerScore>computerScore){
+            resultContainer.textContent="You win!";
+        }
+         else if(computerScore>playerScore){
+            resultContainer.textContent="You lose!";
+        }
+        else{
+            resultContainer.textContent="Tie game!";
+        }
+    }
+
+}
+
+gameV2();
